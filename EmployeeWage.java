56327604995a62172d227585743cb6b338e4d5a0
unknown <press1998@gmail.com>
2020-08-14 18:18:19 +0530

    Added Part time wage calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,15 +4,20 @@ public class EmployeeWage
 	{
 		System.out.println("Welcome to employee wage problem");
 		int fullTime=1;
+		int partTime=2;
 		int empHrs=0;
 		int empWage=0;
 		int wagePerHour=20;
 
-		int randomCheck=(int) (Math.random()*10)%2;
+		int randomCheck=(int) (Math.random()*10)%3;
 
 		if(randomCheck==fullTime)
 		{
 			empHrs=8;
+		}
+		else if(randomCheck==partTime)
+		{
+			empHrs=4;
 		}
 		empWage=wagePerHour*empHrs;
 		System.out.println("Employee daily wage:" + empWage);
