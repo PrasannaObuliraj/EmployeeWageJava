30773011adb888fe8e91b34106c9e06e182f5057
unknown <press1998@gmail.com>
2020-08-18 18:04:31 +0530

    Added procedural way to calculate employee wage

---
+++ day21EmployeeWage.java
@@ -0,0 +1,43 @@
+public class day21EmployeeWage
+{
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+
+	public static void calculateEmployeeWage()
+	{
+		System.out.println("Welcome to employee wage problem");
+                int empRatePerHour=20;
+	        int numberOfWorkingDays=20;
+		int empHrs=0;
+                int totalSalary=0;
+                int totalWorkingDays=0;
+                int totalEmpHrs=0;
+                int totalMaxEmpHrs=100;
+
+                while(totalWorkingDays<numberOfWorkingDays && totalEmpHrs<totalMaxEmpHrs)
+                {
+                        totalWorkingDays++;
+                        int randomCheck=(int) (Math.random()*10)%3;
+                        switch(randomCheck)
+                        {
+                                case FULL_TIME:
+                                        empHrs=8;
+                                        break;
+                                case PART_TIME:
+                                        empHrs=4;
+                                        break;
+                                default:
+                                        empHrs=0;
+                        }
+                        totalEmpHrs=totalEmpHrs+empHrs;
+                }
+                totalSalary=totalEmpHrs*empRatePerHour;
+                System.out.println("Total employee hours:" + totalEmpHrs);
+                System.out.println("Total employee wage:" + totalSalary);
+	}
+
+	public static void main(String[] args)
+	{
+		calculateEmployeeWage();
+	}
+}
